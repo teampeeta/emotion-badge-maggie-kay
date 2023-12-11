3e36939bb095b6e5f552cb032dd724a8a8e8caaf
teampeeta <152302872+teampeeta@users.noreply.github.com>
2023-12-11 10:33:21 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . # . #
+        . # . # .
+        # . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+basic.forever(function () {
+	
+})
